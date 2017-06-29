e64d06bde0778fcaf33a999e640077e67900bc47
angeladowns <angeladowns@me.com>
2017-06-29 12:45:17 +0900

    Set to login to disable Active Admin.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -35,7 +35,7 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
-  config.authentication_keys = [:login]
+    config.authentication_keys = [:login]
 
 
   # Configure parameters from the request object used for authentication. Each entry
