b10f1894236415fa0f96ae4aebf004ec868bf421
angeladowns <angeladowns@me.com>
2018-01-07 15:17:00 -0500

    Fixed Active Admin! Almost.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,30 @@
 class AdminUser < ApplicationRecord
+  has_many :adventures
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  include Gravtastic
+  gravtastic
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, :presence => true, :uniqueness => {:case_sensitive => false } # etc.
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
   attr_accessor :login
+
+
+  def self.find_for_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:login) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
 end
